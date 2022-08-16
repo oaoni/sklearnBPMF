538520ec385aa15f23a4051977f699fa1e02f714
Ola Oni <oa.oni7@gmail.com>
2022-08-16 17:52:14 -0400

    save training and test scores seperately

--- sklearnBPMF/models/regression.py
+++ sklearnBPMF/models/regression.py
@@ -133,6 +133,8 @@ class BayesianRegression:
         train_avg, train_std, train_coord = self.predict(S='train',return_std=True)
 
         #Assign current training metrics
+        self.train_scores = train_dict
+        self.test_scores = test_dict
         self.train_dict = dict(pred_avg = pred_avg,
                                pred_std = pred_std,
                                pred_coord = pred_coord,
