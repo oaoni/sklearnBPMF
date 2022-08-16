3e79f2d7ffcc45bb96f3f718848c4ede725fa5b3
Ola Oni <oa.oni7@gmail.com>
2022-08-16 13:07:08 -0400

    fix variance output

--- sklearnBPMF/models/regression.py
+++ sklearnBPMF/models/regression.py
@@ -79,11 +79,8 @@ class BayesianRegression:
         variance = ((X @ self.cov_) @ (X.T)) + self.sigma
 
         # Format bias and perform transformation
-        if self.bias:
-            if self.bias_both_dim:
-                variance = variance[1:,1:]
-            else:
-                variance = variance[:,1:]
+        if self.bias_both_dim:
+            variance = variance[1:,1:]
 
         return pd.DataFrame(variance + variance.T)
 
